e5308f1c97ad0a0acb33ca2ec1bec50bc221201c
Carol Patricio <87195745+carolina982@users.noreply.github.com>
2025-11-12 10:28:50 -0600

    Reindex proyecto completo y corregido

--- backend/src/config/mailer.ts
+++
@@ -1,9 +0,0 @@
-import nodemailer from "nodemailer";
-
-export const transporter =nodemailer.createTransport ({
-    service:"gmail",
-    auth:{
-        user:"tu_correo@gmail.com",
-        pass:"contraseña"
-    },
-});
--- backend/src/routes/unitRoutes.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import { createUnit, deleteUnit, getUnitById, getUnits, updateUnit } from "../controllers/unitController";
-const router =Router ();
-router.post ("/", createUnit);
-router.get("/", getUnits);
-router.get("/:id" , getUnitById);
-router.put("/:id" ,updateUnit);
-router.delete("/:id" , deleteUnit);
-export default router;
