d2158f5a5f287c83def96ff43074c0aded534ca7
Carol Patricio <87195745+carolina982@users.noreply.github.com>
2026-04-06 13:52:43 -0600

    correcion en backend

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -17,7 +17,13 @@ const app = express();
 const PORT =Number(process.env.PORT) || 3000;
 connectDB();
 
-app.use(cors());
+app.use(cors ({
+    origin:"*",
+    methods:["GET","POST","PUT","DELETE","OPTIONS"],
+    allowedHeaders:["Content-Type","Authorization"],
+}));
+
+app.options("*",cors());
 app.use(express.json());
 app.use("/uploads", express.static(path.join(__dirname,"../uploads")));
 
